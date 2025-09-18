d9dfd608ea627b57c1bcf266a2aafbc1e180393b
Rochdi Nassah <rochdinassah1998@gmail.com>
2025-09-18 10:00:26 +0100

    randomString test

--- tests/helpers.test.js
+++ tests/helpers.test.js
@@ -7,5 +7,10 @@ const rochdi = require('rochdi');
 const { randomString } = rochdi;
 
 test('returns random string', () => {
-  expect(1).toBe(1);
+  const size = 4096;
+  expect(randomString(size).length).toBe(size);
+  expect(randomString(size)).toMatch(/^[a-z0-9]+$/i);
+  expect(randomString(size, { useNumbers: false })).not.toMatch(/\d/i);
+  expect(randomString(size, { useNumbers: false })).not.toMatch(/[.*\\/.]/i);
+  expect(randomString(size, { useNumbers: false, extra: '.*\\/.' })).toMatch(/[.*\\/.]/i);
 });
