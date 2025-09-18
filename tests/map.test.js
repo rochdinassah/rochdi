2519adefa79149ea65afbcdc73bab0e0ea91970a
Rochdi Nassah <rochdinassah1998@gmail.com>
2025-09-18 09:00:26 +0100

    noop, sync with remote

--- tests/map.test.js
+++ tests/map.test.js
@@ -6,5 +6,26 @@
 require('rochdi');
 
 test('Map.prototype.remember test', () => {
+  const map = new Map();
+
+  map.set('foo', 1);
+
+  expect(map.get('foo')).toBe(1);
+  
+  map.remember('foo', 2, 64);
+
+  expect(map.has('foo')).toBe(true);
+  expect(map.get('foo', 2));
+
+  setTimeout(() => {
+    expect(map.has('foo')).toBe(true);
+    expect(map.get('foo')).toBe(2);
+
+    setTimeout(() => {
+      expect(map.has('foo')).toBe(false);
+      expect(map.get('foo')).toBe(void 0);
+    }, 128);
+  }, 32);
+
   expect(1).toBe(1);
 });
