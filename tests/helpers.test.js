b2ae0c51489968ffcdecfc69623d8108bbc0bcfa
Rochdi Nassah <rochdinassah1998@gmail.com>
2025-09-18 09:52:36 +0100

    noop, sync with remote

---
+++ tests/helpers.test.js
@@ -0,0 +1,11 @@
+'use strict';
+
+require('./_matchers');
+
+const rochdi = require('rochdi');
+
+const { randomString } = rochdi;
+
+test('returns random string', () => {
+  expect(1).toBe(1);
+});
