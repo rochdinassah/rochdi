3d9b992e4146b2eced0a79d4deecf7b4dc4ec601
Rochdi Nassah <rochdinassah1998@gmail.com>
2025-09-18 05:15:08 +0100

    add login

--- wifi/LG8245X6-50/index.js
+++ wifi/LG8245X6-50/index.js
@@ -18,8 +18,27 @@ function getRandCount() {
     const { statusCode, data } = res;
     if (200 !== statusCode)
       exit('getRandCount: request error, http(%d)', statusCode);
-    return data.trim() === data;
+    return data.trim()
   });
 }
 
-getRandCount().then(log);
+function login(user, pass) {
+  getRandCount().then(token => {
+    user = encodeURIComponent(user);
+    pass = encodeURIComponent(btoa(pass));
+
+    const url = 'http://192.168.1.1/login.cgi';
+    const body = 'UserName='+user+'&PassWord='+pass+'&Language=english&x.X_HW_Token='+token;
+    const headers = {
+      'cookie': 'Cookie=body:Language:english:id=-1',
+      'accept-encoding': 'gzip, deflate, br',
+      'content-type': 'application/x-www-form-urlencoded'
+    };
+
+    return httpClient.post(url, { headers, body }).then(res => {
+      exit(res);
+    });
+  }); 
+}
+
+login(user, pass);
