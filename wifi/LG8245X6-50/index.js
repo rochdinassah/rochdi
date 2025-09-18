cb768df38435a3dc6a8c558788bfd6bbad2b88aa
Rochdi Nassah <rochdinassah1998@gmail.com>
2025-09-18 05:30:25 +0100

    getToken2

--- wifi/LG8245X6-50/index.js
+++ wifi/LG8245X6-50/index.js
@@ -45,23 +45,25 @@ function login(user, pass) {
       const match = /(CookieHttp\=sid\=[a-z0-9]+\:Language\:english\:id\=1);path\=\/;HttpOnly/.exec(cookie[0]);
       if (!match[1])
         exit('login: did not receive CookieHttp');
-      return log('login ok'), match[1]
+      return log('login ok'), match[1];
     });
   });
 }
 
 function getToken2(cookie) {
-  const url = 'https://192.168.1.1/html/ssmp/accoutcfg/ontmngt.asp';
+  const url = 'http://192.168.1.1/html/ssmp/accoutcfg/ontmngt.asp';
   const headers = {
     'accept-encoding': 'gzip, deflate, br',
     cookie
   };
-  return httpClient.get(url, headers).then(res => {
+  return httpClient.get(url, { headers }).then(res => {
     const { statusCode, data } = res;
     exit(res);
   })
 }
 
-login(user, pass).then(cookie => {
-  
-});
+// login(user, pass).then(cookie => {
+//   getToken2(cookie);
+// });
+
+getToken2('CookieHttp=sid=7897fb0e704ab2d98003b0327d436063b76a58e7cdb056473dbcc469951360d7:Language:english:id=1');
