e1d00082a20ccf80cf9263eece4aa53d9d780ad9
Rochdi Nassah <rochdinassah1998@gmail.com>
2025-09-20 03:23:39 +0100

    cmp test

--- tests/string.test.js
+++ tests/string.test.js
@@ -8,6 +8,15 @@ require('./_matchers');
 
 const sample = 'lorem ipsum foo bar baz qux quux quuz corge grault';
 
+test('comparison test', () => {
+  expect('foo'.cmp('foo')).toBe(true);
+  expect('bar'.cmp('BAR')).toBe(true);
+  expect('baz'.cmp('BaZ')).toBe(true);
+  expect('bAz'.cmp('BaZ')).toBe(true);
+  expect('foo'.cmp('f1o')).toBe(false);
+  expect('bar'.cmp('Bxr')).toBe(false);
+});
+
 test('returns a random character from self', () => {
   expect(sample.rand()).toBeIncluded(sample.split(''));
 });
