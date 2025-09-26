fc9c962ae26342882c85d166a67982bd7ada3bbc
Rochdi Nassah <rochdinassah1998@gmail.com>
2025-09-26 07:55:13 +0100

    noop, sync with remote

--- main.js
+++ main.js
@@ -14,6 +14,7 @@ global.log = console.log.bind(console);
 global.noop = Function.prototype;
 
 module.exports = {
+  Logger: require('./lib/logger'),
   Discord: require('./lib/discord'),
   Server: require('./lib/app/server'),
   Http2Client: require('./lib/http2-client'),
