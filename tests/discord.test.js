a86b1533713b9e827ea7893ab823c96644873016
Rochdi Nassah <rochdinassah1998@gmail.com>
2025-11-22 02:30:38 +0100

    some fixes

--- tests/discord.test.js
+++ tests/discord.test.js
@@ -9,13 +9,32 @@ const rochdi = require('rochdi');
 const { Discord } = rochdi;
 
 const DISCORD_BOT_TOKEN = process.env.DISCORD_BOT_TOKEN;
-
-const discord = new Discord(DISCORD_BOT_TOKEN);
+const DISCORD_USER_TOKEN = process.env.DISCORD_USER_TOKEN;
 
 test('basic connection flow test', async () => {
   if (!true || !DISCORD_BOT_TOKEN)
     return;
 
+  expect(await discord.connect()).toBe(true);
+  expect(await discord.disconnect()).toBe(1001);
+});
+
+test('bot auth', async () => {
+  if (!DISCORD_BOT_TOKEN)
+    return;
+
+  const discord = new Discord(DISCORD_BOT_TOKEN);
+
+  expect(await discord.connect()).toBe(true);
+  expect(await discord.disconnect()).toBe(1001);
+});
+
+test('user auth', async () => {
+  if (!DISCORD_USER_TOKEN)
+    return;
+
+  const discord = new Discord(DISCORD_USER_TOKEN);
+  
   expect(await discord.connect()).toBe(true);
   expect(await discord.disconnect()).toBe(1001);
 });
