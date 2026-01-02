a84d3cc1318666865c397b504bf7ebe567931e77
Rochdi Nassah <rochdinassah1998@gmai.com>
2026-01-02 14:15:35 +0100

    sync

--- app/main.js
+++ app/main.js
@@ -19,4 +19,4 @@ const DIR_PATH = __dirname;
 const addr = String(execSync('hostname -I')).replace(/[\n\r\s]/g, '');
 
 fs.writeFileSync(DIR_PATH+'/raw/addr', addr);
-execSync('git add -A && git commit -m sync && git push');
+execSync('git add -A; git commit -m sync; git push');
