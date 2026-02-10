9a494e2fb7a0f6ce7cb79683aa8153c8a5d16a5a
Rochdi Nassah <rochdinassah1998@gmail.com>
2026-02-10 16:27:16 +0100

    sync

---
+++ test.js
@@ -0,0 +1,15 @@
+'use strict';
+
+const rochdi = require('./');
+
+const { StateManager, CommandManager, HttpClient, Http2Client } = rochdi;
+const { Openai, Logger, Discord, Server, Client } = rochdi;
+
+startTimer('Test');
+
+void async function () {
+  const logger = new Logger({ prefix: 'test' });
+
+  logger.info('test in progress...');
+  logger.info('test complete ok | %s', endTimer('Test'));
+}();
