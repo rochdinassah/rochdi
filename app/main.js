a729049b4b38e21f7eb5e3c28d5577c80ed8d923
Rochdi Nassah <rochdinassah1998@gmail.com>
2026-02-13 23:53:41 +0100

    sync

--- app/main.js
+++ app/main.js
@@ -11,7 +11,7 @@ const DIR_PATH = '/opt/rochdi';
 
 awaitInternet().then(asyncDelay.bind(void 0, 2**12)).then(() => {
   const addr = String(execSync('hostname -I')).replace(/[\n\r\s]/g, '');
-
+  
   writeFileSync(DIR_PATH+'/raw/addr', addr);
   // writeFileSync(DIR_PATH+'/raw/rand', String(rand(2, 999)));
     
