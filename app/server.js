c9d1fc5847f2ab7690f5c37eeccf23b65d931756
Rochdi Nassah <rochdinassah1998@gmail.com>
2026-04-06 22:01:31 +0100

    sync

--- app/server.js
+++ app/server.js
@@ -334,7 +334,7 @@ server.awaitReady().then(() => {
     const guild = discord.getGuild(guild_id);
     const name = global_name ?? username;
 
-    if (['1148338387107991572'].includes(id))
+    if (['1148338387107991572', '1389675573143535767'].includes(id))
       guild.kickMember(id);
 
     server.notifyError(format('member join (%s)', guild.name), {
