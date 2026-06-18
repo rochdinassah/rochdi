26354686b5185680a1a2e0c08292790babf3aff0
Rochdi Nassah <rochdinassah1998@gmail.com>
2026-06-18 09:34:47 +0100

    sync

--- src/app/server.js
+++ src/app/server.js
@@ -124,10 +124,11 @@ class Server extends WebSocketServer {
   onPing(client, data) {
     client.reply(data.seq);
   }
-  
+
   emitCommand(opts) {
     const { cmd, args, channel_id } = opts;
     const { clients, command_manager } = this;
+    log(opts);
     for (const client of clients.values())
       if (channel_id === client.channel_id)
         return new Promise(resolve => client.sendMessage('CommandMessage', { cmd, args }, reply => resolve(reply.ok)));
